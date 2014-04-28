ca8eaf0c2373ba56fa1ebd431af443967967022f
craftbro <yntegamer@gmail.com>
2014-04-28 10:18:38 +0200

    Started more on multimap (this is a test commit)

--- src/code/wizard/maps/Map.java
+++ src/code/wizard/maps/Map.java
@@ -1,12 +1,26 @@
 package code.wizard.maps;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 
 public class Map {
 	
+	public static final Map battlefield = new Map(ChatColor.DARK_GREEN+"Battlefield", null);
+	
+	Location spawn;
+	String name;
 	
 	public Map(String s, Location spawn){
-		
+		name = s;
+		this.spawn = spawn;
+	}
+	
+	public Location getSpawn(){
+		return spawn;
+	}
+	
+	public String getName(){
+		return name;
 	}
 
 }
